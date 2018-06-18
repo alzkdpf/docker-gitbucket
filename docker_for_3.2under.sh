62155b9193fca1ff33654db5c5fcbca1a43fbcdb
Michael L <code21032@gmail.com>
2018-06-18 17:51:19 +0900

    mount volume path fix

--- docker_for_3.2under.sh
+++ docker_for_3.2under.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+if [ ! -d "home" ]; then
+  mkdir home
+fi
+
 docker build -t gitbucket/img:1.0 .
 
 docker run -d \
 --net=host \
 --name=gitbucket \
--v 'home':'/tank/data_dir' \
+-v "$(pwd)/home":"/tank/data_dir" \
 --env-file="./envs/default.env" \
 gitbucket/img:1.0
